cae3c8cb26af4baeddd90b6beb59aebe0bcc637a
mtx512 <jasbirm66@gmail.com>
2024-03-09 12:49:15 +0000

    Add licence

--- include/npu_cna.h
+++ include/npu_cna.h
@@ -1,6 +1,26 @@
 #ifndef NPU_CNA_H
 #define NPU_CNA_H
 
+/*
+ * Copyright (C) 2024  Jasbir Matharu, <jasjnuk@gmail.com>
+ *
+ * This file is part of rk3588-npu.
+ *
+ * rk3588-npu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * rk3588-npu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with rk3588-npu.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <stdint.h>
 
 typedef struct npu_cna_desc {
